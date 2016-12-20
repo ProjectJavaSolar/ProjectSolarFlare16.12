39bb0a998ab8ab922525c393bcb9e97bd402cec1
Nillar <martin.p.nikolov@gmail.com>
2016-12-20 23:39:53 +0200

    cleaned the code a bit, delete unneccessary things

--- src/main/java/space/controller/PlanetController.java
+++ src/main/java/space/controller/PlanetController.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import space.repository.PlanetRepository;
+
 
 @Controller
 public class PlanetController {
-    @Autowired
-    private PlanetRepository planetRepository;
 
     @GetMapping("/mercury")
     public String mercury(Model model){
--- src/main/java/space/repository/PlanetRepository.java
+++
@@ -1,8 +0,0 @@
-package space.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import space.entity.Planet;
-
-public interface PlanetRepository extends JpaRepository<Planet, Integer> {
-}
